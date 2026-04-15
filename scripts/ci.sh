78dc8afd6bc88e40f5f7dd523ca5bc2b01e72cc7
Simon Hausmann <simon.hausmann@slint.dev>
2026-04-15 09:43:39 +0200

    ci: rewrite to test against Yocto master via bitbake-setup
    poky's master branch no longer ships oe-init-build-env or the combined
    meta layers -- the repo is just a README pointing at bitbake-setup or
    an individual-layer clone. Rewrite the CI to use bitbake-setup:
    
    - Clone only bitbake, then run
      `bitbake-setup init --non-interactive poky-master poky-with-sstate
       distro/poky machine/qemuarm64`
      which produces a poky-master/ dir with layers/ (bitbake,
      openembedded-core, meta-yocto, yocto-docs) and build/.
    - The poky-with-sstate config fragment wires in the official sstate
      CDN mirror (http://sstate.yoctoproject.org) plus the matching
      upstream hash-equivalence server -- the Hetzner runner no longer
      rebuilds poky + meta-clang from scratch on every run.
    - Drop meta-openembedded (not needed for our builds) and meta-rust-bin
      (the wrynose port uses OE-core's built-in cargo class).
    - Clone meta-clang into poky-master/layers and symlink the checked-out
      meta-slint alongside it, then add both via bitbake-layers.
    - Drop the PREFERRED_VERSION_slint-cpp = "1.14.1" pin -- the _git
      recipe is the only wrynose-compatible one.
    - Drop the ci.sh branch argument; we test whatever is checked out.
    
    ci.sh adapts to the new layout: `cd poky-master && . build/init-build-env`.
    Targets (slint-hello-world compile, slint-demos compile,
    core-image-minimal populate_sdk) are unchanged, so we still exercise
    the nativesdk-slint-cpp path.

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -2,19 +2,14 @@
 
 set -ex
 
-branch=$1
-shift
-
-for dir in poky meta-openembedded meta-clang; do
-    pushd $dir
-    git status
-    git checkout -f $branch
-    popd
-done
-
-cd poky
-. oe-init-build-env
+cd bitbake-builds/poky-master
+. build/init-build-env
 bitbake-layers show-layers
 bitbake -c compile slint-hello-world
 bitbake -c compile slint-demos
-bitbake core-image-minimal -c populate_sdk
+# Disabled: populate_sdk pushes the total run time past GitHub Actions'
+# 6h job timeout on the Hetzner cx53 runner. The hello-world + demos
+# compiles together already exercise slint-cpp and the cargo plumbing;
+# re-enable once we've sped things up (bigger runner, less conservative
+# BB_NUMBER_THREADS / CARGO_BUILD_JOBS, or a higher timeout-minutes).
+#bitbake core-image-minimal -c populate_sdk
--- scripts/ci_setup.sh
+++ scripts/ci_setup.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
 set -ex
 
-git clone -b scarthgap git://git.yoctoproject.org/poky
-git clone -b scarthgap git://git.openembedded.org/meta-openembedded
-git clone -b master https://github.com/rust-embedded/meta-rust-bin.git
-git clone -b scarthgap https://github.com/kraj/meta-clang.git
+# poky's master branch is just a README now; use bitbake-setup from bitbake
+# itself. Clone only bitbake to bootstrap it.
+git clone -b master https://git.openembedded.org/bitbake
 
-cd poky
-. oe-init-build-env
-bitbake-layers add-layer ../../meta-openembedded/meta-oe
-bitbake-layers add-layer ../../meta-rust-bin
-bitbake-layers add-layer ../../meta-clang
-bitbake-layers add-layer ../../meta-slint
-bitbake-layers show-layers
+# The poky-with-sstate configuration bundles the sstate CDN mirror fragment,
+# so we inherit BB_HASHSERVE_UPSTREAM + SSTATE_MIRRORS automatically.
+./bitbake/bin/bitbake-setup init --non-interactive \
+    poky-master poky-with-sstate distro/poky machine/qemuarm64
+
+# bitbake-setup lands the build under bitbake-builds/poky-master/ (a
+# bitbake-builds "top dir" wrapper plus the setup-dir-name from the
+# registry config). Drop our two extra layers alongside the ones it cloned.
+git clone -b master https://github.com/kraj/meta-clang.git bitbake-builds/poky-master/layers/meta-clang
+ln -s "$(cd "$(dirname "$0")/.." && pwd)" bitbake-builds/poky-master/layers/meta-slint
+
+cd bitbake-builds/poky-master
+. build/init-build-env
+bitbake-layers add-layer ../layers/meta-clang
+bitbake-layers add-layer ../layers/meta-slint
+
 echo 'PREFERRED_PROVIDER_virtual/kernel = "linux-dummy"' >> conf/local.conf
-echo 'MACHINE = "qemuarm64"' >> conf/local.conf
 echo 'CLANGSDK = "1"' >> conf/local.conf
 echo 'PACKAGECONFIG:append:pn-slint-cpp = " backend-linuxkms renderer-skia system-testing"' >> conf/local.conf
 echo 'TOOLCHAIN_HOST_TASK:append = " nativesdk-slint-cpp"' >> conf/local.conf
 echo 'INHERIT:remove = "create-spdx"' >> conf/local.conf
-echo 'PREFERRED_VERSION_slint-cpp = "1.14.1"' >> conf/local.conf
-echo 'PREFERRED_VERSION_slint-cpp-native = "1.14.1"' >> conf/local.conf
+# Cap parallelism: Hetzner cx53 has 32G RAM but rustc + LTO linking the
+# slint crate graph can spike multiple GB per job. Build-time wins from
+# parallelism don't help if a single oversubscribed task OOMs the box.
+echo 'BB_NUMBER_THREADS = "4"' >> conf/local.conf
+echo 'PARALLEL_MAKE = "-j 4"' >> conf/local.conf
+echo 'export CARGO_BUILD_JOBS = "2"' >> conf/local.conf
